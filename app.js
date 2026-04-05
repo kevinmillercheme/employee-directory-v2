02917e8781e8f64a1e7ff4a92def66f1a4cbc2ea
kevinmillercheme <kevinmiller1242@gmail.com>
2026-04-04 22:20:07 -0400

    all functionality, router redirect and POST examples live on site

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
 import express from "express";
+import router from "./routes/employeeRoutes.js";
+
 const app = express();
+
 export default app;
 
-import employees from "#db/employees";
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello employees!");
 });
 
-app.get("/employees", (req, res) => {
-  res.send(employees);
-});
+app.use("/employees", router);
 
-// Note: this middleware has to come first! Otherwise, Express will treat
-// "random" as the argument to the `id` parameter of /employees/:id.
-app.get("/employees/random", (req, res) => {
-  const randomIndex = Math.floor(Math.random() * employees.length);
-  res.send(employees[randomIndex]);
-});
-
-app.get("/employees/:id", (req, res) => {
-  const { id } = req.params;
-
-  // req.params are always strings, so we need to convert `id` into a number
-  // before we can use it to find the employee
-  const employee = employees.find((e) => e.id === +id);
-
-  if (!employee) {
-    return res.status(404).send("Employee not found");
-  }
-
-  res.send(employee);
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong");
 });
---
+++ routes/employeeRoutes.js
@@ -0,0 +1,47 @@
+import express from "express";
+import employees from "../db/employees.js";
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send(employees);
+});
+
+router.get("/random", (req, res) => {
+    const randomIndex = Math.floor(Math.random() * employees.length);
+    res.send(employees[randomIndex]);
+});
+
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    const employee = employees.find((employee) => employee.id === +id);
+
+    if (!employee) {
+        return res.status(404).send("Employee not found");
+    }
+
+    res.send(employee);
+});
+
+router.post("/", (req, res) => {
+    if (!req.body || !req.body.name) {
+        return res.status(400).send("Name is required");
+    }
+
+    const { name } = req.body;
+
+    if (!name) {
+        return res.status(400).send("Name is required");
+    }
+
+    const newEmployee = {
+        id: employees.length + 1,
+        name,
+    };
+
+    employees.push(newEmployee);
+
+    res.status(201).send(newEmployee);
+});
+
+export default router;
